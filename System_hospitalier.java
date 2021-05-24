bacba92b1b1ba9f7820cb474b7d2fe2db2d37cc0
Xavier <petit2coly@gmail.com>
2021-05-25 01:10:19 +0200

    Premier commit dans la branche Groupe6_NiangFatim

---
+++ System_hospitalier.java
@@ -0,0 +1,47 @@
+package tp_git;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class System_hospitalier {
+	
+	private List<Requête> listRequetes;
+
+	public System_hospitalier(List<Requête> listRequetes) {
+		super();
+		this.listRequetes = listRequetes;
+	}
+
+	public System_hospitalier() {
+		super();
+		listRequetes = new ArrayList<>();
+	}
+
+	public List<Requête> getListRequetes() {
+		return listRequetes;
+	}
+
+	public void setListRequetes(List<Requête> listRequetes) {
+		this.listRequetes = listRequetes;
+	}
+	
+	public void recevoirRequete(Requête requete) {
+		listRequetes.add(requete);
+		String analyse =  analyserRequete(requete);
+		String comparaisonDonnees = comparerDonnes();
+		System.out.println(analyse);
+		System.out.println(comparaisonDonnees);
+	}
+	
+	private String analyserRequete(Requête requête) {
+		// Analyser la requête et retourne la requ�te analys�e
+		return "requête analysée";
+	}
+	
+	private String comparerDonnes() {
+		// Comparaison des données
+		//Elle retourne "Cluster trouv�" � la fin de la comparaison
+		return "Cluster trouvé";
+	}
+
+}
